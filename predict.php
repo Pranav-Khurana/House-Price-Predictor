039f0d29121a6a95e79c1ef588f5c3a545bdaedf
Pranav-Khurana <apranavkhurana18@gmail.com>
2019-07-23 11:33:47 +0530

    added all project files

---
+++ predict.php
@@ -0,0 +1,26 @@
+<?php 
+//Get Parameters
+
+$lotsize = $_GET["lotsize"];
+$bathrms = $_GET["bathrms"];
+$stories = $_GET["stories"];
+$driveway = $_GET["driveway"];
+$recroom = $_GET["recroom"];
+$fullbase = $_GET["fullbase"];
+$gashw = $_GET["gashw"];
+$airco = $_GET["airco"];
+$garagepl = $_GET["garagepl"];
+$prefarea = $_GET["prefarea"];
+
+//Execute the Model
+system("/usr/anaconda/bin/python3 house_price_model.py ".$lotsize." ".$bathrms." ".$stories." ".$driveway." ".$recroom." ".$fullbase." ".$gashw." ".$airco." ".$garagepl." ".$prefarea." 2>&1");
+
+?>
+
+
+
+
+
+
+
+
